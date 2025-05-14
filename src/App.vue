d8c2adba100b14f2aa303c0641f6936b633ec954
Эдуард Андреев <4ar@MacBook.local>
2025-05-14 21:22:28 +0300

    Забыл добавить локаль

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
+<template>
+  {{ date }}
+</template>
+
+
 <script setup>
 import { ref } from 'vue'
 const date = ref(
-  new Date().toLocaleDateString({
+  new Date().toLocaleDateString('ru-RU', {
     year: 'numeric',
     month: 'numeric',
     day: 'numeric',
   }),
 )
 </script>
-
-<template>
-  {{ date }}
-</template>
 
 <style scoped></style>
