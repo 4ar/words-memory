20c1716a6315e41ec70d8259849edbcae9ca32fe
Эдуард Андреев <4ar@MacBook.local>
2025-05-15 20:19:37 +0300

    Компонент кнопки

---
+++ src/components/ButtonPrimary.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+<template>
+  <button class="btn-primary">
+    <slot />
+  </button>
+</template>
+<style scoped>
+.btn-primary {
+  display: block;
+  background-color: var(--primaryColor);
+  color: var(--white);
+  border-radius: 100px;
+  border: 0;
+  font-weight: 400;
+  font-size: 24px;
+  padding: 16px 10px;
+  width: 335px;
+  height: 68px;
+  cursor: pointer;
+}
+
+.btn-primary:hover {
+  background-color: var(--primaryColorHover);
+}
+</style>
